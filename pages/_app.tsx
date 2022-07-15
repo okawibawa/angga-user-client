fcfdd67d37e5b51b7876c3876ca88b163d8b5916
Oka Wibawa <okaa.wibawa@gmail.com>
2022-07-15 13:46:03 +0800

    updates font

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,12 @@
 import type { AppProps } from 'next/app';
 import { ChakraProvider } from '@chakra-ui/react';
+import theme from '../styles/theme';
 import '@fontsource/poppins';
 import '@fontsource/poppins/600.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <Component {...pageProps} />
     </ChakraProvider>
   );
