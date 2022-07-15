4baabcd7bc3e416c1e57e3ffd0cb267f3524f743
Oka Wibawa <okaa.wibawa@gmail.com>
2022-07-15 13:41:23 +0800

    First commit

---
+++ styles/theme.ts
@@ -0,0 +1,20 @@
+// theme.ts (tsx file with usage of StyleFunctions, see 4.)
+import { extendTheme } from '@chakra-ui/react';
+import { StyleFunctionProps } from '@chakra-ui/theme-tools';
+
+const breakpoints = {
+  sm: '37.5em', // 600px
+  md: '56.25', // 900px
+  lg: '75em', // 1200px
+  xl: '112.5em', // 1800px
+};
+
+const theme = extendTheme({
+  breakpoints,
+  fonts: {
+    heading: `'Poppins', sans-serif`,
+    body: `'Poppins', sans-serif`,
+  },
+});
+
+export default theme;
